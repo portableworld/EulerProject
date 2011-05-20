23856ba593af4d7c6fbb9a340e51b151a4ae9395
Charles Caldwell <bookedge@gmail.com>
2011-05-19 21:07:15 -0500

    First Commit - Solved Problems 1 and 2

---
+++ src/eulerproject/EulerProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eulerproject;
+import eulerproject.Problems.*;
+
+/**
+ *
+ * @author Charlie
+ */
+public class EulerProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Problem_2 problem = new Problem_2();
+        System.out.println(problem.Solve());
+    }
+}
---
+++ src/eulerproject/Problems/Problem_3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eulerproject.Problems;
+
+/**
+ *
+ * @author Charlie
+ */
+public class Problem_3 {
+    /**
+     * Problem 3 ---
+     * 
+     */
+    
+    public String Solve() {
+        
+        return "The solution is: ";
+    }
+}
